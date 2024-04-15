a9531a7511456d7b40f5db0190e62d31a6a3a92e
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-04-15 20:31:49 +0000

    aboutpath

--- backend/Files_modules/aboutpath.js
+++ backend/Files_modules/aboutpath.js
@@ -1,3 +1,11 @@
-import path from "path"
+import path from "path";
 
-console.log(path);
+// console.log(path);
+
+let myPath = "/workspaces/BackEnd-practice/backend/Files_modules/harry.txt";
+
+console.log(path.extname(myPath));
+console.log(path.dirname(myPath));
+console.log(path.basename(myPath));
+
+console.log(path.join("workspaces:/", "backend/harry.txt"));

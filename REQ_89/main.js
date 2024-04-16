1493a5710d4b48bf9c95a36f37872b4b9b76e830
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-04-16 23:22:58 +0000

    req

--- REQ_89/main.js
+++ REQ_89/main.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log("hey its a get request");
+  res.send('Hello World2!')
+})
+app.post('/', (req, res) => {
+  console.log("hey its a get request");
+  res.send('Hello World post!')
 })
 
 app.listen(port, () => {
